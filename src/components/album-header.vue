0ef0db8c62d2f1c023be084d5329c7b7e48f7649
Allen Garvey <allengarvey@hotmail.com>
2022-11-01 08:47:21 -0400

    Fix album description wrapping on desktop

--- src/components/album-header.vue
+++ src/components/album-header.vue
@@ -8,33 +8,38 @@
                 loading="lazy"
             />
         </div>
-        <div>
-        <table :class="$style.table">
-            <tr>
-                <td><h3 :class="$style.title">{{ album.meta.title }}</h3></td>
-                <td :class="$style.secondaryInfo">{{ yearDescriptionForAlbum(album) }}</td>
-            </tr>
-            <tr>
-                <td>{{ album.meta.artist }}</td>
-                <td :class="$style.secondaryInfo">{{ album.meta.tags.join(', ') }}</td>
-            </tr>
-            <tr v-if="album.meta.description">
-                <td rowspan="2">
-                    <ul :class="$style.description">
-                        <li v-for="row in album.meta.description">{{ row }}</li>
-                    </ul>
-                </td>
-            </tr>
-        </table>
+        <div :class="$style.infoContainer">
+            <table :class="$style.table">
+                <tr>
+                    <td><h3 :class="$style.title">{{ album.meta.title }}</h3></td>
+                    <td :class="$style.secondaryInfo">{{ yearDescriptionForAlbum(album) }}</td>
+                </tr>
+                <tr>
+                    <td>{{ album.meta.artist }}</td>
+                    <td :class="$style.secondaryInfo">{{ album.meta.tags.join(', ') }}</td>
+                </tr>
+                <tr v-if="album.meta.description">
+                    <td rowspan="2">
+                        <ul :class="$style.description">
+                            <li v-for="row in album.meta.description">{{ row }}</li>
+                        </ul>
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
 </template>
     
 <style lang="scss" module>
+    $cover-image-width: 220px;
+
     .container {
         margin-bottom: 2rem;
         display: flex;
         flex-wrap: wrap;
+    }
+    .infoContainer {
+        flex-basis: calc(100% - #{$cover-image-width});
     }
     .table {
         padding: 0 1rem;
@@ -51,8 +56,8 @@
         padding-left: 1rem;
     }
     .coverImage {
-        width: 220px;
-        height: 220px;
+        width: $cover-image-width;
+        height: $cover-image-width;
     }
     .description {
         list-style-type: none;
