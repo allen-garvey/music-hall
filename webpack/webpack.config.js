b3b1d217763657adb3c58d1da406a8a5db214ffb
Allen Garvey <allengarvey@hotmail.com>
2022-11-16 08:43:51 -0500

    Disable typechecking on server build to get rid of spurious type error

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,4 @@
 const serverConfig = require('./server.config');
 const clientConfig = require('./client.config');
 
-module.exports = [serverConfig({isWatch: false}), clientConfig({isWatch: false})];
+module.exports = [serverConfig({isWatch: true}), clientConfig({isWatch: false})];
